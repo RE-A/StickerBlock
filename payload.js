f9c0f623bec51532c6906a577a0d01278b2ea133
RE-A <skyseven73@gmail.com>
2019-05-31 13:40:06 +0900

    Alpha Release

--- payload.js
+++ payload.js
@@ -1,11 +1,46 @@
-// send the page title as a chrome message
-// https://blog.naver.com/webhouse73/220496881108
-// https://blog.naver.com/ssyj963/221505966350
+var currenturl = location.href;
 
-var stickerArrayList = document.getElementsByClassName('se-sticker-image');
-$(".se-sticker-image").hide()
-sendingMeg = JSON.parse(JSON.stringify(stickerArrayList.length));
+if(currenturl.indexOf("blog.naver.com") != -1){
+    var mainFrame = document.getElementsByName('mainFrame');
+    if(mainFrame[0] != null){
+        var innerHtmlUrl = mainFrame[0].src;
+        location.href = innerHtmlUrl;
+    }
+    else{
+        hidestiker();
+    }
+}
+else{
+    var frameset = document.getElementsByName('screenFrame');
+    if(frameset[0]!=null){
+        var blogUrl = frameset[0].src;
+        location.href = blogUrl;
+    }
+}
 
-chrome.runtime.sendMessage(sendingMeg);
-    
-    
+function hidestiker(){
+    var stickerNum = 0 ;
+
+    // img 형식으로 올라온 스티커를 제거하는 파트
+    pics = document.getElementsByTagName('img');
+    var re = /https\:\/\/storep-phinf\.pstatic\.net.*/
+    for(var i = 0 ; i<pics.length;i++ ){
+        imgsrc = pics[i].src;
+        if(re.exec(imgsrc)){
+            stickerNum++;
+            pics[i].style.display='none';
+        }
+    }
+
+    // sticker 클래스로 올라온 스티커를 제거하는 파트
+    var stickerArrayList = document.getElementsByClassName('se-sticker-image');
+    if(stickerArrayList.length > 0){
+        $(".se-sticker-image").hide()
+        stickerNum += stickerArrayList.length;
+    }
+
+    if(stickerNum>0){
+        sendingMeg = JSON.parse(JSON.stringify(stickerNum));
+        chrome.runtime.sendMessage(sendingMeg);
+    }
+}
